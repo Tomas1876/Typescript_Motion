de7e4f24ecf93ab52f05dd70ae60dbace8187f68
Tomas1876 <mnd3259@gmail.com>
2021-09-10 14:45:37 +0900

    BaseComponent 생성, 리팩토링

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,12 +1,8 @@
-export class PageComponent{
-    private element:HTMLUListElement;
-    constructor(){
-        this.element = document.createElement('ul');
-        this.element.setAttribute('class','page');
-        this.element.textContent = 'This is PageComponent';
-    }
+import { BaseComponent } from './../component.js';
 
-    attachTo(parent:HTMLElement, position:InsertPosition = 'afterbegin'){//position 값이 들어오지 않으면 기본적으로 afterbegin
-        parent.insertAdjacentElement(position, this.element);
+export class PageComponent extends BaseComponent<HTMLUListElement>{
+    constructor(){
+        super(`<ul class='page'>This is PageComponent</ul>`)
     }
+   
 }
