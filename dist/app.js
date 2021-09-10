36c59ba90d72186811e620336074e346d504ed64
Tomas1876 <mnd3259@gmail.com>
2021-09-10 16:53:53 +0900

    item Components

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,20 @@
+import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
+import { VideoComponent } from './components/page/item/video.js';
 import { PageComponent } from './components/page/page.js';
+import { TodoComponent } from './components/page/item/todo.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
         const image = new ImageComponent('Image Title', 'https://picsum.photos/seed/picsum/600/300');
         image.attachTo(appRoot, 'beforeend');
+        const note = new NoteComponent('Note title', 'Note body');
+        note.attachTo(appRoot, 'beforeend');
+        const todo = new TodoComponent('Todo title', 'Todo');
+        todo.attachTo(appRoot, 'beforeend');
+        const video = new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=I6fP5a-Z0yg/600/300');
+        video.attachTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
+import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
+import { VideoComponent } from './components/page/item/video.js';
 import { PageComponent } from './components/page/page.js';
+import { TodoComponent } from './components/page/item/todo.js';
 
 class App{
     private readonly page: PageComponent;
@@ -8,7 +11,13 @@ class App{
         this.page.attachTo(appRoot) //만들어진 page를 appRoot에 붙여준다
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/seed/picsum/600/300');
-        image.attachTo(appRoot, 'beforeend') //마지막에 추가
+        image.attachTo(appRoot, 'beforeend') 
+        const note = new NoteComponent('Note title', 'Note body');
+        note.attachTo(appRoot, 'beforeend');
+        const todo = new TodoComponent('Todo title', 'Todo');
+        todo.attachTo(appRoot, 'beforeend');
+        const video = new VideoComponent('Video Title','https://www.youtube.com/watch?v=I6fP5a-Z0yg/600/300');
+        video.attachTo(appRoot, 'beforeend')
     }
 }
 
